6434bda9a027b8a29c28ae8d559eeb781030209e
Pedro Costa <pedrodc23@gmail.com>
2018-04-02 18:06:09 +0100

    Updated prod config

--- apps/app/config/prod.exs
+++ apps/app/config/prod.exs
@@ -1,3 +1,12 @@
 use Mix.Config
 
+
+config :app, App.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATA_DB_USER"),
+  password: System.get_env("DATA_DB_PASS"),
+  hostname: System.get_env("DATA_DB_HOST"),
+  database: "gonano",
+  pool_size: 10
+
 import_config "prod.secret.exs"
