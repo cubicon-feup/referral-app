fbd0f7cf56bf6d6c9c3e8ad99d16148ca406ecfe
Pedro Costa <pedrodc23@gmail.com>
2018-04-05 15:16:58 +0100

    Added User model

---
+++ apps/app/lib/app/users/user.ex
@@ -0,0 +1,24 @@
+defmodule App.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+
+  schema "user" do
+    field :date_of_birth, :date
+    field :deleted, :boolean, default: false
+    field :email, :string
+    field :name, :string
+    field :password, :string
+    field :picture_path, :string
+    field :priveleges_level, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:date_of_birth, :email, :name, :password, :picture_path, :priveleges_level, :deleted])
+    |> validate_required([:date_of_birth, :email, :name, :password, :picture_path, :priveleges_level, :deleted])
+  end
+end
