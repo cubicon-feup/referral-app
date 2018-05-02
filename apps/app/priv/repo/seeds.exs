c922fa35496bec3d1bb33fa7e1389249354f186d
Pedro Costa <pedrodc23@gmail.com>
2018-05-02 16:25:13 +0100

    Small improvements

--- apps/app/priv/repo/seeds.exs
+++ apps/app/priv/repo/seeds.exs
@@ -13,10 +13,10 @@
 
 
 user_create_attrs = %{date_of_birth: ~D[2010-04-17], deleted: false, email: "mail@mail.com", name: "John Doe", password: "password", picture_path: nil, privileges_level: "user"}
-brand_create_attrs = %{api_key: "e1c4afd5632958dd66626a3257ac72d7", api_password: "b36782cbdc2d7991f2c804bcd63a9246", hostname: "duarte-store-29.myshopify.com", name: "Brandify"}
-
-
 {:ok, user} = App.Users.create_user(user_create_attrs)
+
+
+brand_create_attrs = %{api_key: "e1c4afd5632958dd66626a3257ac72d7", api_password: "b36782cbdc2d7991f2c804bcd63a9246", hostname: "duarte-store-29.myshopify.com", name: "Brandify", user_id: user.id}
 {:ok, brand} = App.Brands.create_brand(brand_create_attrs)
 
 influencer_create_attrs = %{address: "Rua Dr.Roberto Frias", nib: 258963147, name: "John Doe", user_id: user.id}
