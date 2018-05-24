9a8332ef6321b36e4232e7c281c886d806f2ed71
Pedro Costa <pedrodc23@gmail.com>
2018-05-24 19:06:11 +0100

    Commented unnused function

--- apps/app_web/lib/app_web/controllers/user_controller.ex
+++ apps/app_web/lib/app_web/controllers/user_controller.ex
@@ -140,7 +140,7 @@ defmodule AppWeb.UserController do
 
 
             if upload = user_params["photo"] do
-              File.exists?(upload.path)
+              #File.exists?(upload.path)
               extension = Path.extname(upload.filename)
               File.cp(upload.path, "./apps/app_web/assets/static/images/media/#{user.id}-profile#{extension}")
             end
