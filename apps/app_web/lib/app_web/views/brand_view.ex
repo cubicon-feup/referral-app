072e8c5629892214cc2e49a39e72612f6cc630b4
Pedro Costa <pedrodc23@gmail.com>
2018-05-25 15:28:58 +0100

    Added metrics to influencers page

--- apps/app_web/lib/app_web/views/brand_view.ex
+++ apps/app_web/lib/app_web/views/brand_view.ex
@@ -6,4 +6,16 @@ defmodule AppWeb.BrandView do
   def get_influencer_revenue(contract_id) do
     Contracts.get_total_contract_revenue(contract_id)
   end
+
+  def get_influencer_pending_payments(contract_id) do
+    Contracts.get_contract_pending_payments(contract_id)
+  end
+
+  def get_influencer_voucher_views(contract_id) do
+    Contracts.get_total_contract_views(contract_id)
+  end
+
+  def get_number_of_influencer_sales(contract_id) do
+    Contracts.get_number_of_sales(contract_id)
+  end
 end
