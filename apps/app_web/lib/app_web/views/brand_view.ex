6e5ef2d0b1fbbca8fd1817db37bb843afbd84899
Pedro Costa <pedrodc23@gmail.com>
2018-05-25 14:52:29 +0100

    Working on influencers listing

--- apps/app_web/lib/app_web/views/brand_view.ex
+++ apps/app_web/lib/app_web/views/brand_view.ex
@@ -1,3 +1,9 @@
 defmodule AppWeb.BrandView do
   use AppWeb, :view
+
+  alias App.Contracts
+
+  def get_influencer_revenue(contract_id) do
+    Contracts.get_total_contract_revenue(contract_id)
+  end
 end
